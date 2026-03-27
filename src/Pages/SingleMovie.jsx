7e7c56d9ecb5bb01bc378d6e981c49e83ba648ce
Pushkrine Pratap Singh <pushkrine77@gmail.com>
2026-03-27 13:18:21 +0000

    Watchlist Page Added,Trailer links for movies are added

--- src/Pages/SingleMovie.jsx
+++ src/Pages/SingleMovie.jsx
@@ -4,7 +4,6 @@ import { baseImageUrl } from "../data";
 import { FaBookmark, FaStar } from "react-icons/fa";
 import { ImCancelCircle } from "react-icons/im";
 
-
 import "./SingleMovie.css";
 import { MovieContext } from "../Components/Router";
 
@@ -13,7 +12,7 @@ function SingleMovie() {
   const [movie, setMovie] = useState(null);
   const [trailerKey, setTrailer] = useState(null);
   const [showTrailer, setShowTrailer] = useState(false);
-  let {AddToWatchlist}=useContext(MovieContext);
+  let { AddToWatchlist } = useContext(MovieContext);
 
   useEffect(() => {
     fetchMovie();
@@ -25,7 +24,7 @@ function SingleMovie() {
     const API_KEY = import.meta.env.VITE_API_KEY;
 
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`,
     );
 
     const result = await response.json();
@@ -41,13 +40,13 @@ function SingleMovie() {
     const API_KEY = import.meta.env.VITE_API_KEY;
 
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`,
     );
 
     const data = await res.json();
 
     const trailer = data.results.find(
-      (vid) => vid.type === "Trailer" && vid.site === "YouTube"
+      (vid) => vid.type === "Trailer" && vid.site === "YouTube",
     );
 
     if (trailer) {
@@ -61,22 +60,20 @@ function SingleMovie() {
   return (
     <div className="page">
       <div className="single-movie">
-
         <div className="left">
-          <img
-            src={`${baseImageUrl}${movie.poster_path}`}
-            alt={movie.title}
-          />
+          <img src={`${baseImageUrl}${movie.poster_path}`} alt={movie.title} />
         </div>
 
         <div className="right">
           <h1>{movie.title}</h1>
 
-          <p className="overview">{movie.overview}</p>
-
-          <p className="release">
-            Release Date: {movie.release_date}
+          <p className="overview">
+            <span>Description : </span>
+            <br />
+            {movie.overview}
           </p>
+
+          <p className="release">Release Date: {movie.release_date}</p>
 
           <p className="rating">
             Rating:
@@ -87,26 +84,20 @@ function SingleMovie() {
             {showTrailer ? "Close Trailer" : "Watch Trailer"}
           </button>
 
-          <button className="watchlist-btn" onClick={()=>AddToWatchlist(movie)}>
+          <button
+            className="watchlist-btn"
+            onClick={() => AddToWatchlist(movie)}
+          >
             <FaBookmark /> Add To Watchlist
           </button>
         </div>
       </div>
 
       {showTrailer && trailerKey && (
-        <div
-          className="trailer-modal"
-          onClick={() => setShowTrailer(false)}
-        >
-          <div
-            className="trailer-content"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <span
-              className="close-btn"
-              onClick={() => setShowTrailer(false)}
-            >
-              <ImCancelCircle/>
+        <div className="trailer-modal" onClick={() => setShowTrailer(false)}>
+          <div className="trailer-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close-btn" onClick={() => setShowTrailer(false)}>
+              <ImCancelCircle />
             </span>
 
             <iframe
